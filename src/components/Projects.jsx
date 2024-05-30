826ede5ffacc1cd38eacc7b719033584536dcd77
Manikanta954 <talagamamanikanta98@gmail.com>
2024-05-30 21:42:43 +0530

    added projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,26 +1,31 @@
-// src/components/Certifications.js
 import React from 'react';
 import './Projects.css';
 
-const certifications = [
-  { id: 1, title: 'Certificate 1', imageUrl: 'path/to/certificate1.jpg' },
-  { id: 2, title: 'Certificate 2', imageUrl: 'path/to/certificate2.jpg' },
-  { id: 3, title: 'Certificate 3', imageUrl: 'path/to/certificate3.jpg' },
-  { id: 4, title: 'Certificate 4', imageUrl: 'path/to/certificate4.jpg' },
-  // Add more certificates as needed
+const projects = [
+  { id: 1, title: 'Project 1', description: 'Description of Project 1', imageUrl: 'path/to/certificate1.jpg' },
+  { id: 2, title: 'Project 2', description: 'Description of Project 2', imageUrl: 'path/to/certificate2.jpg' },
+  { id: 3, title: 'Project 3', description: 'Description of Project 3', imageUrl: 'path/to/certificate3.jpg' },
+  { id: 4, title: 'Project 4', description: 'Description of Project 4', imageUrl: 'path/to/certificate4.jpg' },
+  // Add more projects as needed
 ];
 
-const Certifications = () => {
+const Projects = () => {
   return (
-    <div className="certifications-grid">
-      {certifications.map((cert) => (
-        <div key={cert.id} className="cert-box">
-          <img src={cert.imageUrl} alt={cert.title} className="cert-image" />
-          <div className="cert-title">{cert.title}</div>
+    <div>
+       <h1 style={{paddingLeft:'30px',fontSize:'40px'}}>My Projects</h1>
+    <div className="projects-grid">
+     
+      {projects.map((proj) => (
+        <div key={proj.id} className="proj-box">
+          <img src={proj.imageUrl} alt={proj.title} className="proj-image" />
+          <div className="proj-info">
+            <h3 className="proj-title">{proj.title}</h3>
+            <p className="proj-description">{proj.description}</p>
+          </div>
         </div>
       ))}
-    </div>
+    </div></div>
   );
 };
 
-export default Certifications;
+export default Projects;
