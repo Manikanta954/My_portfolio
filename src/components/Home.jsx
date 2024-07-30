b7c9b4b552b6fb5f17ea3e07ed059e51a4c718f9
Manikanta954 <talagamamanikanta98@gmail.com>
2024-07-31 00:15:30 +0530

    Made it responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,14 @@ import me1 from './me2.png';  // Make sure this path is correct
 const Home = () => (
   <div className="home-container">
     <div className="text-container">
-      <h1 style={{fontSize:"40px",color:'White'}}>Welcome!<span className="waving-hand">👋</span></h1>
+      <h1 style={{fontSize:"40px",color:'White'}} className='welcome-text'>Welcome!<span className="waving-hand">👋</span></h1>
       <h1 style={{fontSize:"40px",color:'White'}}>I am <span style={{color:'#11f670'}}>
         <span style={{fontSize:"50px", padding:"5px"}}>M</span>anikanta <span style={{fontSize:"50px"}}>T</span>alagama</span></h1>
       <p style={{fontSize:"35px", color:'white',paddingRight:"15px"}}>I am a <span style={{color:'#11f670'}}> 
         <ReactTypingEffect text={['Python programmer.', 'Java programmer.','Flutter Developer.','React Developer.']} speed={100} eraseSpeed={50} eraseDelay={2000} typingDelay={500} />
       </span></p>
+      <a href="/contact-me" class="hire-me-button">Hire Me</a>
+
     </div>
     <div className="image-container">
       <img src={me1} alt="Manikanta Talagama" className="profile-image" />
