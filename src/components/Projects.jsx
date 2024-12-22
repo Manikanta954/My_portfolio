e740a48c41375ce1bde19acb172614d7070d0967
Manikanta954 <talagamamanikanta98@gmail.com>
2024-12-22 16:12:41 +0530

    added project2 image output

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Projects.css';
 import project1 from '../Assets/project1.jpg';
-
+import project2 from '../Assets/project2.png';
 const projects = [
   { id: 1, title: 'Project 1', description: 'A beautiful ecommerce web app for farm products with full fuctionality and real time usage.', imageUrl: project1, link: 'https://organic-egg-farm.vercel.app/' },
-  { id: 2, title: 'Stock Price Pridiction', description: 'Description of Project 2', imageUrl: 'path/to/certificate2.jpg', link: 'https://github.com/Manikanta954/Stock-price-predictor' },
+  { id: 2, title: 'Electronics shopping app', description: 'A Flutter-based app with attractive UI and smooth animations using velocity x library', imageUrl:project2, link: 'https://github.com/Manikanta954/simple-electronics-shopping-app' },
   { id: 3, title: 'Speech Emotion Detection using ML', description: 'Description of Project 3', imageUrl: 'path/to/certificate3.jpg', link: 'https://example.com/project3' },
   { id: 4, title: 'Vegies Shopping app using flutter', description: 'Description of Project 4', imageUrl: 'path/to/certificate4.jpg', link: 'https://github.com/Manikanta954/Vegetables-shopping-app' },
   // Add more projects as needed
